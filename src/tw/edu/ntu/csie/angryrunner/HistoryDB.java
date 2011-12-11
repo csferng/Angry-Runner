840118499c098be7e8a2ca6f0bfb87a5ba85d376
Hsueh-Wen Hsiu <b95902057@ntu.edu.tw>
2011-12-11 22:43:30 +0800

    Add DB for History. History page AC. (Need insert db row in Stop button listener in WorkoutActivity.)

---
+++ src/tw/edu/ntu/csie/angryrunner/HistoryDB.java
@@ -0,0 +1,35 @@
+package tw.edu.ntu.csie.angryrunner;
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteDatabase.CursorFactory;
+import android.database.sqlite.SQLiteOpenHelper;
+
+
+public class HistoryDB extends SQLiteOpenHelper {
+
+	public HistoryDB(Context context) {
+		super(context, "AngryRunnerHistoryDB", null, 1);
+	}
+
+	@Override
+	public void onCreate(SQLiteDatabase db) {
+		db.execSQL("CREATE TABLE ARhistory (mode TEXT, date TEXT, " +
+				"distance TEXT, duration TEXT, speed TEXT)");
+		/*
+		ContentValues testcv = new ContentValues();
+		testcv.put("mode", "walking");
+		testcv.put("date", "Jan 1, 2012");
+		testcv.put("distance", "1.0 km");
+		testcv.put("duration", "0:00:00");
+		testcv.put("speed", "0.0 m/s");
+		db.insert("ARhistoryTEST", null, testcv);
+		*/
+	}
+
+	@Override
+	public void onUpgrade(SQLiteDatabase arg0, int arg1, int arg2) {
+		// TODO Auto-generated method stub
+	}
+
+}
